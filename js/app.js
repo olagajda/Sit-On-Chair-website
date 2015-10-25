68b5a5f8f88ce8db92b6ba9981055890fdc47045
olagajda <aleksandra.gajda@gmail.com>
2015-10-25 22:36:19 +0100

    added prices table functionality

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ $(function(){
 
 
 
-    // Hiding see-through panels on photogrsphs:
+    // Hiding see-through panels on photographs:
 
     var boxes = $(".box12");
 
@@ -137,44 +137,79 @@ $(function(){
 
 
 
-    // Dropdown compose-your-chair form:
+    // Dropdown compose-your-chair form and prices table:
 
 
     var formArrow = $(".blue-arrow");
+    var dropdownChoice = $(".dropdown li");
+    var empty = 0;
+    $("#chair-price").text(empty);
+    $("#color-price").text(empty);
+    $("#fabric-price").text(empty);
+
+
+
 
     formArrow.on("click", function(event){
 
-        console.log("hejki");
-
         $(this).parent().parent().find("dd").slideToggle(500);
 
+    });
+
+
+    var empty = 0;
+    $("#chair-price").text(empty).css("color", "white");
+    $("#color-price").text(empty).css("color", "white");
+    $("#fabric-price").text(empty).css("color", "white");
+
+    dropdownChoice.on("click", function(event){
+
+
+        console.log("hejki");
+        var chairChoice = $(this).data("chair");
+        var colorChoice = $(this).data("color");
+        var fabricChoice = $(this).data("fabric");
+        var chairPrice = $(this).data("chair-price");
+        var colorPrice = $(this).data("color-price");
+        var fabricPrice = $(this).data("fabric-price");
+
+        $(this).parents().find("#chair-name").text(chairChoice);
+        $(this).parents().find("#chair-price").text(chairPrice).css("color", "#27c7ab");
+        $(this).parents().find("#chair-color").text(colorChoice);
+        $(this).parents().find("#color-price").text(colorPrice).css("color", "#27c7ab");
+        $(this).parents().find("#chair-fabric").text(fabricChoice);
+        $(this).parents().find("#fabric-price").text(fabricPrice).css("color", "#27c7ab");
+
+        $(this).parent().parent().parent().find("dt span").text($(this).text());
+
+        $(this).parent().parent().slideUp(500);
+
+        $(this).parents().find("#chair-sum").text( ( parseInt($(this).parents().find("#chair-price").text()) ) + ( parseInt($(this).parents().find("#transport-price p").text()) ) + ( parseInt($(this).parents().find("#color-price").text()) ) + ( parseInt($(this).parents().find("#fabric-price").text()) )).css("color", "#27c7ab").show(0);
 
 
     });
 
 
+    $('#transport').click(function() {
+        $(this).parents().find("#transport-yes p").toggle();
+        $(this).parents().find("#transport-price p").toggle();
 
 
+        var text =  $(this).parents().find("#transport-price p").text();
+
+        console.log(text);
+
+        if (text === "0") {
+            $(this).parents().find("#transport-price p").text("200") ;
+        }
+        else {
+            $(this).parents().find("#transport-price p").text("0");
+        }
+
+        $(this).parents().find("#chair-sum").text( ( parseInt($(this).parents().find("#chair-price").text()) ) + ( parseInt($(this).parents().find("#transport-price p").text()) ) + ( parseInt($(this).parents().find("#color-price").text()) ) + ( parseInt($(this).parents().find("#fabric-price").text()) )).css("color", "#27c7ab").show(0);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
 
 
 });
